c332ae565fcd32addd96e5ac05a1c925ff3a6ea5
99050823 <99050823@mydavinci.nl>
2020-12-01 13:12:46 +0100

    CafeApp
    Verbetering

--- CaféApp.js
+++ CaféApp.js
@@ -9,12 +9,12 @@ var wijn = 0;
 var bitterballen8 = 0;
 var bitterballen16 = 0;
 
-//drinken
+//drinken prijzen
 const prijs1 = 2.00;
 const prijs2 = 2.60;
 const prijs3 = 1.80;
 
-//bitterballen 
+//bitterballen prijzen
 const prijs4 = 4.00;
 const prijs5 = 8.00;
 
@@ -50,15 +50,15 @@ function keuzemenu () {
 function KeuzeBitterballen () {
 
     while (keuze2 != "stop") {
-        keuze2 = prompt("Bitterballen \nType hier uw keuze: 8 of 16");
+        keuze2 = prompt("Bitterballen \nType hier uw keuze: 8 of 16 \nKies: 8st = 1, 16st = 2");
 
-        if (keuze2 == "8") {
+        if (keuze2 == "1") {
             keuze2 = 1;
             bitterballen8+=keuze2;
 
             console.log(bitterballen8);
 
-        } else if(keuze2 == "16") {
+        } else if(keuze2 == "2") {
             keuze2 = 1;
             bitterballen16+=keuze2;
 
@@ -69,23 +69,52 @@ function KeuzeBitterballen () {
 
 function kassabon () {
 
-    document.write("Aantal glazen bier: " + bier + "<br>")
-    document.write("Aantal glazen wijn: " + wijn + "<br>")
-    document.write("Aantal glazen fris: " + fris + "<br>")
+    if (bier > 0) {
+        document.write("Aantal glazen bier: " + bier + "<br>")
+    }
+
+    if (wijn > 0) {
+        document.write("Aantal glazen wijn: " + wijn + "<br>")
+    }
+
+    if (fris > 0) {
+        document.write("Aantal blikken fris: " + fris + "<br>")
+    }
 
     document.write("<br>")
 
-    document.write("Aantal bitterballen 8st: " + bitterballen8 + "<br>");
-    document.write("Aantal bitterballen 16st: " + bitterballen16 + "<br>");
+    if (bitterballen8 > 0) {
+        document.write("Aantal bitterballen 8st: " + bitterballen8 + "<br>");
+    }
+
+    if (bitterballen16 > 0) {
+        document.write("Aantal bitterballen 16st: " + bitterballen16 + "<br>");
+    }
+
+    document.write("-------")
+    document.write("<br>")
+
+    if (bier > 0) {
+        document.write("Prijs bier: &euro;" + bier * prijs1 + "<br>");
+    } 
+
+    if (wijn > 0) {
+        document.write("Prijs wijn: &euro;" + wijn * prijs2 + "<br>");
+    } 
+
+    if (fris > 0 ) {
+        document.write("Prijs fris: &euro;" + fris * prijs3 + "<br>");
+    } 
 
     document.write("<br>")
 
-    document.write("prijs bier: &euro;" + bier * prijs1 + "<br>");
-    document.write("prijs wijn: &euro;" + wijn * prijs2 + "<br>");
-    document.write("prijs fris: &euro;" + fris * prijs3 + "<br>");
+    if (bitterballen8 > 0) {
+        document.write("Prijs bitterballen 8st: &euro;" + bitterballen8 * prijs4 + "<br>");
+    }
 
-    document.write("Prijs bitterballen 8st: &euro;" + bitterballen8 * prijs4 + "<br>");
-    document.write("Prijs bitterballen 16st: &euro;" + bitterballen16 * prijs5 + "<br>");
+    if (bitterballen16 > 0) {
+        document.write("Prijs bitterballen 16st: &euro;" + bitterballen16 * prijs5 + "<br>");
+    }
 
     document.write("<br>");
 
